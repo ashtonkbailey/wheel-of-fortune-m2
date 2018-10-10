827eca3d3142dfca1c523eea2b4baf8a76a14a74
Ashton Bailey <ashtonkbailey@gmail.com>
2018-10-10 15:02:13 -0600

    Fix issue #5

---
+++ Puzzle.js
@@ -0,0 +1,14 @@
+class Puzzle {
+	constructor() {
+		this.guessedLetter = guessedLetter;
+	}
+
+	showLetter() {
+		//if guessed letter matches answer, show letter
+	}
+
+	showAnswer() {
+		//if puzzle is solved correctly, show answer
+	}
+
+}
