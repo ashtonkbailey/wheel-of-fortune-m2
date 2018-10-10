4f12969c21ea4768d199d20e0a98edf59754fe28
Ashton Bailey <ashtonkbailey@gmail.com>
2018-10-10 15:42:20 -0600

    Fix issue #7 and #11 and update puzzle methods

---
+++ BonusRound.js
@@ -0,0 +1,11 @@
+class BonusRound extends Round {
+	constructor(currCategory, currWheel, currPlayer) {
+		super(currCategory);
+		this.bonusWheel;
+	}
+
+	getBonusWheel() {
+		//call bonusWheel from Wheel
+	}
+
+}
