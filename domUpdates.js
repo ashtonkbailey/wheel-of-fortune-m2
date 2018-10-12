91c714afd12b133e15286d6c5128222c6336b45a
Ashley Levi <Ashplevi@gmail.com>
2018-10-11 18:34:57 -0600

    Add function to update player names on home page

--- domUpdates.js
+++ domUpdates.js
@@ -6,15 +6,24 @@ const domUpdates = {
 	  startContainer.classList.add('hide');
 	  game = new Game;
 	  game.startGame();
+    updatePlayerNames();
+
 	},
 
 	displayCategory() {
 		let category = puzzle.currentPuzzle.category;
-
 		categoryDisplay.innerText = category;
 	}
 
 };
+
+
+function updatePlayerNames() {
+  playerOneName.innerText = nameOneInput.value;
+  playerTwoName.innerText = nameTwoInput.value;
+  playerThreeName.innerText = nameThreeInput.value;
+  
+}
 
 
 if (typeof module !== 'undefined') {
